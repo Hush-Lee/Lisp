441323f4baefd32b328e2e1f822f069e34b5891e
Hush-Lee <liyu981203@163.com>
2025-03-03 23:45:13 +0800

    day61

--- CLisp/learn.lisp
+++ CLisp/learn.lisp
@@ -79,3 +79,23 @@
 	      (cur 0 next)
 	      (next 1 (+ cur next)))
 	     ((= f n) cur)))
+
+(defun primep (number)
+  (when (> number 1)
+    (loop for fac from 2 to (isqrt number) never (zerop (mod number fac)))))
+
+(defun next-prime (number)
+  (loop for n from number when (primep n) return n))
+
+(defmacro my-do-primes (p from a to b &rest body)
+  `(do ((,p (next-prime ,a) (next-prime (1+ ,p))))
+       ((> ,p ,b))
+     ,@body))
+
+(defmacro do-primes (var-and-range &rest body)
+  (let ((var (first var-and-range))
+	(start (second var-and-range))
+	(end (third var-and-range)))
+    `(do ((,var (next-prime ,start) (next-prime (1+ ,var))))
+	 ((> ,var ,end))
+       ,@body)))
